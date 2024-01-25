2ff4d91c5a1dd726cd22572d86f7848a2f2323a4
NicCarp11 <nic.carp11@gmail.com>
2024-01-25 19:46:14 +0100

    upgrade for interleaved + output channel

--- counter_instr.py
+++ counter_instr.py
@@ -4,36 +4,43 @@ import re
 def counter(infile, outfile):
     with open(infile) as f:
         reader = csv.reader(f)
-        counts = {'NOP': {}, 'INSTR': {}, 'PERC_INSTR': {}}
+        counts = {
+            'NOP': {}, 
+            'LWD': {}, 
+            'SWD': {}, 
+            'LWI': {}, 
+            'SWI': {},
+            'ALU': {}  # Aggiunta della categoria ALU
+        }
         current_timestamp = None
 
         for row in reader:
-            # Se la riga contiene un timestamp
             match = re.match(r"(\d+)", row[0])
             if match:
                 current_timestamp = match.group(1)
-                # Inizializza i conteggi per questo timestamp
                 if current_timestamp not in counts['NOP']:
-                    counts['NOP'][current_timestamp] = 0
-                    counts['INSTR'][current_timestamp] = 0
+                    for key in counts:
+                        counts[key][current_timestamp] = 0  # Inizializzazione per tutte le categorie
             else:
-                # Aggiorna i conteggi
                 counts['NOP'][current_timestamp] += row.count("NOP")
-                counts['INSTR'][current_timestamp] += len(row) - row.count("NOP")
-
-        # Calcolo delle percentuali
-        for timestamp in counts['NOP']:
-            total = counts['NOP'][timestamp] + counts['INSTR'][timestamp]
-            if total > 0:
-                counts['PERC_INSTR'][timestamp] = (counts['INSTR'][timestamp] / total)
-            else:
-                counts['PERC_INSTR'][timestamp] = 0
+                for instr in row:
+                    if instr == "NOP":
+                        continue  # Escludi NOP dal conteggio ALU
+                    elif re.match(r"LWD\s+\w+", instr):
+                        counts['LWD'][current_timestamp] += 1
+                    elif re.match(r"SWD\s+\w+", instr):
+                        counts['SWD'][current_timestamp] += 1
+                    elif re.match(r"LWI\s+\w+,\s*\w+", instr):
+                        counts['LWI'][current_timestamp] += 1
+                    elif re.match(r"SWI\s+\w+,\s*\w+", instr):
+                        counts['SWI'][current_timestamp] += 1
+                    else:
+                        counts['ALU'][current_timestamp] += 1  # Aggiornamento del conteggio ALU
 
     # Formattazione dell'output
     out_string = "T\t" + "\t".join(counts['NOP'].keys()) + "\n"
-    out_string += "NOP\t" + "\t".join(str(counts['NOP'][key]) for key in counts['NOP']) + "\n"
-    out_string += "INSTR\t" + "\t".join(str(counts['INSTR'][key]) for key in counts['INSTR']) + "\n"
-    out_string += "PERC_INSTR\t" + "\t".join(f"{counts['PERC_INSTR'][key]:.4f}" for key in counts['PERC_INSTR']) + "\n"
+    for key in counts:
+        out_string += key + "\t" + "\t".join(str(counts[key][timestamp]) for timestamp in counts[key]) + "\n"
 
     with open(outfile, "w") as f:
         f.write(out_string)
