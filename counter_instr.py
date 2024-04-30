c1111b1c88f53d6d6a813538fd0d18fd4f7c81fa
NicCarp11 <nic.carp11@gmail.com>
2024-04-30 12:47:12 +0200

    Organized files and removed unnecessary ones

--- counter_instr.py
+++ counter_instr.py
@@ -6,11 +6,11 @@ def counter(infile, outfile):
         reader = csv.reader(f)
         counts = {
             'NOP': {}, 
-            'LWD': {}, 
-            'SWD': {}, 
-            'LWI': {}, 
-            'SWI': {},
-            'ALU': {}  # Aggiunta della categoria ALU
+            'LW': {},  # Unifica LWD e LWI in LW
+            'SW': {},  # Unifica SWD e SWI in SW
+            'ADD': {},  # Ora include anche SSUB
+            'MUL': {},  # Categoria MUL
+            'OTHERS': {}  # Categoria per le istruzioni non specificate
         }
         current_timestamp = None
 
@@ -18,24 +18,24 @@ def counter(infile, outfile):
             match = re.match(r"(\d+)", row[0])
             if match:
                 current_timestamp = match.group(1)
-                if current_timestamp not in counts['NOP']:
+                if current_timestamp not in counts:
                     for key in counts:
-                        counts[key][current_timestamp] = 0  # Inizializzazione per tutte le categorie
+                        counts[key][current_timestamp] = 0  # Inizializzazione dei conteggi
             else:
                 counts['NOP'][current_timestamp] += row.count("NOP")
                 for instr in row:
                     if instr == "NOP":
-                        continue  # Escludi NOP dal conteggio ALU
-                    elif re.match(r"LWD\s+\w+", instr):
-                        counts['LWD'][current_timestamp] += 1
-                    elif re.match(r"SWD\s+\w+", instr):
-                        counts['SWD'][current_timestamp] += 1
-                    elif re.match(r"LWI\s+\w+,\s*\w+", instr):
-                        counts['LWI'][current_timestamp] += 1
-                    elif re.match(r"SWI\s+\w+,\s*\w+", instr):
-                        counts['SWI'][current_timestamp] += 1
+                        continue
+                    elif re.match(r"LW[D|I]\s+\w+", instr) or re.match(r"LW[D|I]\s+\w+,\s*\w+", instr):
+                        counts['LW'][current_timestamp] += 1
+                    elif re.match(r"SW[D|I]\s+\w+", instr) or re.match(r"SW[D|I]\s+\w+,\s*\w+", instr):
+                        counts['SW'][current_timestamp] += 1
+                    elif "ADD" in instr or "SSUB" in instr:  # Ora include SSUB nella categoria ADD
+                        counts['ADD'][current_timestamp] += 1
+                    elif "MUL" in instr:
+                        counts['MUL'][current_timestamp] += 1
                     else:
-                        counts['ALU'][current_timestamp] += 1  # Aggiornamento del conteggio ALU
+                        counts['OTHERS'][current_timestamp] += 1  # Tutte le altre istruzioni
 
     # Formattazione dell'output
     out_string = "T\t" + "\t".join(counts['NOP'].keys()) + "\n"
@@ -44,3 +44,4 @@ def counter(infile, outfile):
 
     with open(outfile, "w") as f:
         f.write(out_string)
+
